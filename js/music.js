8c468e6a919035a08112fcb372e850ab762e8813
xram3827 <xram3827@giant.cos.edu>
2025-03-23 18:21:32 -0700

    Update music.js
    transfer from codepen

--- js/music.js
+++ js/music.js
@@ -1,3 +1,14 @@
 function playMusic() {
     alert("Playing Marvin Gaye's Music!");
 }
+// Get the button
+let mybutton = document.getElementById("scrollTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "block";
+    }
+};
